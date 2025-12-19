b8395919ed62893e3720ed8d125b8c139fd8264d
Hans Müller <iraqihu1994@gmail.com>
2025-12-19 17:17:43 +0100

    Added images need for the atf area. Implemented the first version of aft with placeholder image.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Atf } from './atf/atf';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, Atf],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('portfolio');
--- src/app/atf/atf.ts
+++ src/app/atf/atf.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { Navbar } from '../navbar/navbar';
 
 @Component({
   selector: 'app-atf',
-  imports: [],
+  imports: [Navbar],
   templateUrl: './atf.html',
   styleUrl: './atf.scss',
 })
-export class Atf {
-
-}
+export class Atf {}
