7bffc3266e22bc0c278ad23bf54bf8e24643944c
Hans Müller <iraqihu1994@gmail.com>
2026-01-08 17:47:46 +0100

    Added icons for current skills. Also adjusted navbar to be sticky instead of position: fixed and added the why-me-section to the app.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Atf } from './atf/atf';
+import { WhyMeSection } from './why-me-section/why-me-section';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, Atf],
+  imports: [RouterOutlet, CommonModule, Atf, WhyMeSection],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
