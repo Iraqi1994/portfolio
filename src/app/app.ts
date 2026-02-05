38918202c92f632970b5111476f9ee2fdde662b8
Hans Müller <iraqihu1994@gmail.com>
2026-02-05 19:09:37 +0100

    Created my-skills component.

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Atf } from './atf/atf';
 import { WhyMeSection } from './why-me-section/why-me-section';
+import { MySkills } from './my-skills/my-skills';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, Atf, WhyMeSection],
+  imports: [RouterOutlet, CommonModule, Atf, WhyMeSection, MySkills],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
