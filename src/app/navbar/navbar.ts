dce4b28634ace0ba2934b9bb094ece50f2c2702e
Hans Müller <iraqihu1994@gmail.com>
2026-02-12 16:54:18 +0100

    Added scrolling to section when clicking on respective navitems. Also gave navitems an ellipse background to further highlight on which section the user is currently at.

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -5,7 +5,45 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
+  host: {
+    '(window:scroll)': 'onWindowScroll()',
+  },
 })
 export class Navbar {
+  isSticky = false;
+  lastScrollPosition = 0;
+  activeSection = '';
 
+  onWindowScroll() {
+    const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+    const viewportHeight = window.innerHeight;
+
+    if (currentScrollPosition > viewportHeight - 104) {
+      this.isSticky = true;
+    } else {
+      this.isSticky = false;
+      this.activeSection = '';
+    }
+
+    if (this.isSticky) {
+      let foundSection = false;
+      const sections = ['whyMe', 'mySkills', 'myProjects'];
+      for (const sectionId of sections) {
+        const element = document.getElementById(sectionId);
+        if (element) {
+          const rect = element.getBoundingClientRect();
+          if (rect.top <= 150 && rect.bottom >= 150) {
+            this.activeSection = sectionId;
+            foundSection = true;
+            break;
+          }
+        }
+      }
+      if (!foundSection) {
+        this.activeSection = '';
+      }
+    }
+
+    this.lastScrollPosition = currentScrollPosition;
+  }
 }
