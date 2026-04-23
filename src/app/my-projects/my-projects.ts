4e1879449ee64c89ada275e32b8b6a6a2029ade0
Hans Müller <iraqihu1994@gmail.com>
2026-04-23 13:05:24 +0200

    Changed the favicon. Added an ongoing project called iot-dashboard.

--- src/app/my-projects/my-projects.ts
+++ src/app/my-projects/my-projects.ts
@@ -36,8 +36,7 @@ export class MyProjects {
       'It revolutionizes team communication and collaboration with its intuitive interface and powerful features 2.',
     workProcess:
       'Lorem ipsum dolor sit, amet consectetur adipisicing elit. At cumque voluptate natus placeat unde laboriosam quam officia ducimus sunt, molestias eveniet dolores exercitationem maiores tempore aspernatur. Expedita ratione fugit voluptatibus?',
-    workExperience:
-      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. At cumque voluptate natus placeat unde laboriosam quam officia ducimus sunt, molestias eveniet dolores exercitationem maiores tempore aspernatur. Expedita ratione fugit voluptatibus?',
+    workExperience: '',
     projectImage: 'img/screenshot-dabubble.png',
   };
 
@@ -49,5 +48,14 @@ export class MyProjects {
     workExperience:
       'Lorem ipsum dolor sit, amet consectetur adipisicing elit. At cumque voluptate natus placeat unde laboriosam quam officia ducimus sunt, molestias eveniet dolores exercitationem maiores tempore aspernatur. Expedita ratione fugit voluptatibus?',
     projectImage: 'img/screenshot-dabubble.png',
+  };
+
+  iotDashboardDetails: projectDetailsType = {
+    aboutTheProject:
+      'This data-intensive web app combines commissioning and monitoring of a PV system. It provides live values of production and consumption and tracks errors before the customer notices them. It is a unified dashboard that allows every employee to monitor or depending on their role to even perform actions like OS updates or rebooting the system.',
+    workProcess:
+      'The vision was to develop one single application that unifies all tasks instead of maintaining multiple apps for each domain. The most important part was the research. It was critical to identify what stakeholders need in order to combine those needs.',
+    workExperience: '',
+    projectImage: 'img/iot-dashboard.png',
   };
 }
